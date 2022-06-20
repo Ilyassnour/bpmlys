a511331a2fac38c040e6a3fc07b1ff7409a07369
Ilyassnour <97604281+Ilyassnour@users.noreply.github.com>
2022-06-19 18:18:16 -0700

    Update login.blade.php
    tass7i7

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master2')
 
 @section('title')
-تسجيل الدخول - مورا سوفت للادارة القانونية
+login/signup
 @stop
 
 
@@ -11,14 +11,14 @@
 @endsection
 @section('content')
 		<div class="container-fluid">
-			<div class="row no-gutter">
+	
 				<!-- The image half -->
 				<!-- The content half -->
-				<div class="col-md-6 col-lg-6 col-xl-5 bg-white">
+				<div >
 					<div class="login d-flex align-items-center py-2">
 						<!-- Demo content-->
 						<div class="container p-0">
-							<div class="row">
+						
 								<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
 									<div class="card-sigin">
 										<div class="mb-5 d-flex"> <a href="{{ url('/' . $page='Home') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Suivez<span>Comptes</span>BPM</h1></div>
@@ -64,6 +64,10 @@
                                                     {{ __('Se connecter') }}
                                                     </button>
 												</form>
+
+												
+													 <a href="/signup" class="btn btn-main-primary btn-block"
+           style="color:black"><i class=""></i> &nbsp;signup</a>
 											</div>
 										</div>
 									</div>
@@ -73,13 +77,7 @@
 					</div>
 				</div><!-- End -->
 
-                <div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
-					<div class="row wd-100p mx-auto text-center">
-						<div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
-							<img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
-						</div>
-					</div>
-				</div>
+              
 
 			</div>
 		</div>
